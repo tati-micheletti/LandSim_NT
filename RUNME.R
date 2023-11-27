22cbbcc9608442460d6f46c94bf8a78b5d4712bd
Tati Micheletti <tati.micheletti@gmail.com>
2023-11-27 19:43:52 +0000

    Updated Global -- fixed urls, branches. Working until FS run

--- RUNME.R
+++ RUNME.R
@@ -19,14 +19,12 @@ out <- SpaDES.project::setupProject(
                scratchPath = "~/scratch"),
   modules =
     file.path("PredictiveEcology",
-              c("canClimateData@canadianProvs",
-                paste0(# terra-migration
-                  c("Biomass_speciesData",
+              c(paste0(# development
+                  c("canClimateData",
+                    "Biomass_core",
+                    "Biomass_speciesData",
                     "Biomass_borealDataPrep",
-                    "Biomass_core"),
-                  "@terra-migration"),
-                paste0(# development
-                  c("Biomass_speciesFactorial",
+                    "Biomass_speciesFactorial",
                     # "Biomass_speciesParameters", # Stalls, and we don't need for now
                     "fireSense_dataPrepFit",
                     "fireSense_IgnitionFit",
@@ -36,30 +34,43 @@ out <- SpaDES.project::setupProject(
                     "fireSense_IgnitionPredict",
                     "fireSense_EscapePredict",
                     "fireSense_SpreadPredict"),
-                  "@development")
+                  "@development")#,
+              # "canClimateData@usePrepInputs"
               )),
   functions = "tati-micheletti/LandSim_NT@main/R/outterFuns.R",
   options = list(spades.allowInitDuringSimInit = TRUE,
+                 reproducible.cacheSaveFormat = "rds",
                  gargle_oauth_email = if (user("tmichele")) "tati.micheletti@gmail.com" else NULL,
                  SpaDES.project.fast = TRUE,
-                 reproducible.inputPaths = if (user("tmichele")) "~/data" else NULL),
+                 reproducible.inputPaths = if (user("tmichele")) "~/data" else NULL,
+                 reproducible.useMemoise = TRUE),
   times = list(start = 2011,
                end = 2100),
-  studyArea = studyAreaGenerator(url = "https://drive.google.com/file/d/1RPfDeHujm-rUHGjmVs6oYjLKOKDF0x09", 
+  studyArea = reproducible::Cache(studyAreaGenerator,url = "https://drive.google.com/file/d/1RPfDeHujm-rUHGjmVs6oYjLKOKDF0x09", 
                                  archive = "NT1_BCR6.zip",
                                  targetFile = "NT1_BCR6.shp",
                                  destPath = paths[["inputPath"]]),
-  rasterToMatch = rtmGenerator(url = "https://drive.google.com/file/d/11yCDc2_Wia2iw_kz0f0jOXrLpL8of2oM",
-                               sA = studyArea, destPath = paths[["inputPath"]]),
-  studyAreaLarge = studyAreaGenerator(large = TRUE, destPath = paths[["inputPath"]]),
-  rasterToMatchLarge = rtmGenerator(sA = studyAreaLarge, destPath = paths[["inputPath"]]),
+  rasterToMatch = reproducible::Cache(rtmGenerator, url = "https://drive.google.com/file/d/11yCDc2_Wia2iw_kz0f0jOXrLpL8of2oM",
+                               sA = studyArea, 
+                               destPath = paths[["inputPath"]]),
+  studyAreaLarge = reproducible::Cache(studyAreaGenerator, url = "https://drive.google.com/file/d/1RPfDeHujm-rUHGjmVs6oYjLKOKDF0x09",
+                                      archive = "NT1_BCR6.zip",
+                                      targetFile = "NT1_BCR6.shp",
+                                      large = TRUE, 
+                                      destPath = paths[["inputPath"]]),
+  rasterToMatchLarge = reproducible::Cache(rtmGenerator, sA = studyAreaLarge, 
+                                    destPath = paths[["inputPath"]],
+                                    large = TRUE, 
+                                    tags = "RTMlarge"),
   sppEquiv = sppEquiv_CA(runName),
   # fireSense_spreadFormula = "~ 0 + youngAge + MDC + class2 + class3 + nonForest_highFlam + nonForest_lowFlam", # Being created by fireSense_dataPrepFit
   params = list(.globals = list(sppEquivCol = runName,
                                 dataYear = "2011",
                                 .plots = NA,
                                 .plotInitialTime = NA,
-                                .useCache = c(".inputObjects", "init")),
+                                .useCache = c(".inputObjects", "init")), # Cache seems to be working only for Biomass_speciesData, Biomass_speciesFactorial,Biomass_borealDataPrep, Biomass_core,canClimateData, fireSense_dataPrepFit, fireSense_SpreadFit, fireSense_dataPrepPredict
+                fireSense_EscapePredict = list(.useCache = FALSE),
+                fireSense_IgnitionPredict = list(.useCache = FALSE),
                 fireSense_SpreadFit = list(# Values from WBI
                 # maxAsymptote, hillSlope1, inflectionPoint1, MDC, youngAge, class2, class3, nonForest_highFlam, nonForest_lowFlam
                                            lower = c(0.25, 0.2, 0.1, c(0, rep(-4, times = 5))), 
@@ -69,11 +80,11 @@ out <- SpaDES.project::setupProject(
                                       climateSSP = "370",
                                       historicalFireYears = 1991:2020)
   ),
-  packages = c("googledrive", 'RCurl', 'XML',
+  packages = c("googledrive", 'RCurl', 'XML', 'igraph', 'qs',
                "PredictiveEcology/LandR@development (>= 1.1.0.9074)",
-               "PredictiveEcology/SpaDES.core@optionsAsArgs (>= 2.0.2.9010)",
-               "PredictiveEcology/reproducible@reproducibleTempCacheDir (>= 2.0.8.9012)",
-               "PredictiveEcology/climateData@stopMessageHelp (>= 1.0.4)",
+               "PredictiveEcology/SpaDES.core@development (>= 2.0.3.9000)",
+               "PredictiveEcology/reproducible@development (>= 2.0.10)",
+               "PredictiveEcology/climateData@development (>= 1.0.4)",
                "PredictiveEcology/fireSenseUtils@development (>= 0.0.5.9053)"),
   useGit = "sub"
 )
